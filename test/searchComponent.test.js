2fb9558f77532ffb65488715e11856f7e4359766
Kin Chan <xkinchanx@gmail.com>
2018-12-24 17:24:54 -0800

    [test] ability to create a team

--- test/searchComponent.test.js
+++ test/searchComponent.test.js
@@ -1,5 +1,5 @@
 import app from '../server/index';
-import { Board } from '../database/schema';
+import { Board, Team } from '../database/schema';
 import supertest from 'supertest';
 
 describe('Search Component: ', () => {
@@ -12,12 +12,20 @@ describe('Search Component: ', () => {
     expect(body).toHaveProperty('cards');
   });
 
-  test('it should add to the DB', async () => {
-    const { status } = await supertest(app).post('/api/board').send({ title: 'jest' });
+  test('it should add a new board to the DB', async () => {
+    const { status } = await supertest(app).post('/api/board').send({ title: 'jestBoard' });
     expect(status).toEqual(201);
-    const { body } = await supertest(app).get('/api/board?title=jest');
-    expect(body.title).toEqual('jest');
-    Board.destroy({ where: { title: 'jest' }});
+    const { body } = await supertest(app).get('/api/board?title=jestBoard');
+    expect(body.title).toEqual('jestBoard');
+    Board.destroy({ where: { title: 'jestBoard' }});
+  });
+
+  test('it should add a new team to the DB', async () => {
+    const { status } = await supertest(app).post('/api/team').send({ teamname: 'jestTeam' });
+    expect(status).toEqual(201);
+    await Team.findOne({ where: { teamname: 'jestTeam' }})
+      .then(result => expect(result.teamname).toEqual('jestTeam'))
+    Team.destroy({ where: { teamname: 'jestTeam' }});
   });
 
 
