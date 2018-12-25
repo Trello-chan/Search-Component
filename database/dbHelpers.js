28516b7384fcbd96a27de7944afb2204d9e1e499
Kin Chan <xkinchanx@gmail.com>
2018-12-24 21:29:11 -0800

    [prog] helper to find card that's like provided text

--- database/dbHelpers.js
+++ database/dbHelpers.js
@@ -1,3 +1,4 @@
+import Sequelize from 'sequelize';
 import SQL_connection from './index';
 import { Member, Board, Team, Card, Card_Member } from './schema';
 
@@ -5,23 +6,23 @@ const getRandomMemberHelper = () => Member.findById(Math.ceil(Math.random() * 10
 
 const getRandom15BoardsHelper = query => Board.findOne(query); //either randomId for initial load or name for subsequent query
 
+const getRandom10TeamsHelper = () => Team.findAll({ order: SQL_connection.random(), limit: 10 });
+
+const getRandom50CardsHelper = () => Card.findAll({ order: SQL_connection.random(), limit: 50 }); //join card_member to member
+
 const createBoardHelper = title => Board.create({ title });
 
 const getBoardHelper = title => Board.findOne({ where: { title }});
 
-const getRandom10TeamsHelper = () => Team.findAll({ order: SQL_connection.random(), limit: 10 });
-
 const createTeamHelper = teamname => Team.create({ teamname });
 
-const getRandom50CardsHelper = () => Card.findAll({ order: SQL_connection.random(), limit: 50 }); //join card_member to member
-
-const getCardHelper = query => 
-  Card.findOne({ 
-    label: { $like: `\%${query}\%`},
-    include: [{
-      model: Member
-    }]
-  }); //join card_member to member
+const getCardHelper = label => 
+  Card.findOne({
+    where: {
+      label: { [Sequelize.Op.like]: `%${label}%` }
+    }
+  }); 
+//join card_member to member
 
 const updateCardHelper = (id, update) =>
   Card.update(
