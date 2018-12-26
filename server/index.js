a31e92461a2595506b8acec310891dbcb3e103f1
Kin Chan <xkinchanx@gmail.com>
2018-12-25 16:10:51 -0800

    [fix] for cannot GET/

--- server/index.js
+++ server/index.js
@@ -9,9 +9,11 @@ const port = process.env.PORT || 3001;
 
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../client/dist/index.html')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.use('/api', router)
+
+app.use('*', (req, res) => res.sendFile(path.resolve(__dirname, '../client/dist/index.html')));
 
 if (process.env.NODE_ENV !== 'jest') {
   app.listen(port, () => console.log(`server is listening on port ${port}`));
