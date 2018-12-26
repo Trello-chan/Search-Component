d875f197902bdb95ee3dba6b6bd672128d702caf
Kin Chan <xkinchanx@gmail.com>
2018-12-25 19:52:03 -0800

    [prog] some additional search component structure

---
+++ client/src/components/SearchService.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+
+class SearchService extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <StyledSearchComponent>
+        <GlobalStyle />
+        <StyledButton>
+          {/* home button */}
+        </StyledButton>
+        {/* boards dropdown */}
+        {/* cards/board search */}
+        {/* trello-chan logo */}
+        <StyledButton>
+          {/* create button */}
+        </StyledButton>
+        <StyledButton>
+          {/* info button */}
+        </StyledButton>
+        {/* notifications button */}
+        {/* member logo */}
+      </StyledSearchComponent>
+    )
+  }
+}
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    height: 100vh;
+    margin: 0px;
+  }
+`
+
+const StyledSearchComponent = styled.div`
+  background: rgba(0,0,0,.35);
+  height: 24px;
+  padding: 4px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+
+const StyledButton = styled.div`
+  background: rgba(255,255,255,.3);
+  border-radius: 3px;
+  height: 100%;
+  width: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 4px;
+`
+
+export default SearchService;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import App from './components/App';
+import SearchService from './components/SearchService';
 
-render(<App />, document.getElementById('app'));
+render(<SearchService />, document.getElementById('searchService'));
