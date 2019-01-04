14666616eca8b55648d3b3feb8b0ad39634a14b8
Kin Chan <xkinchanx@gmail.com>
2019-01-04 14:31:49 -0800

    [refactor] moving out top container up one level for reuse

--- client/src/components/Search/SearchDrawerDefault.jsx
+++ client/src/components/Search/SearchDrawerDefault.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const SearchDrawerDefault = (props) =>
-  <DrawerContainer>
+  <div>
     <DrawerTop>
       <div>SAVED SEARCHES</div>
       <div>My Cards @me</div>
@@ -13,29 +13,15 @@ const SearchDrawerDefault = (props) =>
       <div>Refine your search with operators like @member, #label, is:archived, and has:attachments.</div>
       <div>Learn More...</div>
     </DrawerBottom>
-  </DrawerContainer>
-
-const DrawerContainer = styled.div`
-  background-color: #ebeef0;
-  border-radius: 2px;
-  color: #6b808c;
-  font-family: Helvetica Neue,Arial,Helvetica,sans-serif;
-  font-size: 11px;
-  left: 106px;
-  min-height: 125px;
-  position: absolute;
-  top: 34px;
-  width: 460px;
-  padding: 10px;
-`;
+  </div>
 
 const DrawerTop = styled.div`
-  & :nth-child(1) {
+  > :nth-child(1) {
     font-size: 9px;
     font-weight: 550;
     letter-spacing: 0.07em;
   }
-  & :nth-child(2) {
+  > :nth-child(2) {
     padding: 12px 0px 12px 7px;
   }
 `
@@ -45,18 +31,18 @@ const DrawerBottom = styled.div`
   flex-direction: row;
   padding: 5px;
   line-height: 16px;
-  & :nth-child(n) {
+  > * {
     padding: 1px;
   }
-  & :nth-child(1) {
+  > :nth-child(1) {
     background: url(https://a.trellocdn.com/prgb/dist/images/empty-states/comb.65864547b3e6ae50d7ff.svg) no-repeat 0;
     height: 40px;
     width: 42px;
   }
-  & :nth-child(2) {
+  > :nth-child(2) {
     width: 74%;
   }
-  & :nth-child(3) {
+  > :nth-child(3) {
     line-height: 38px;
     text-decoration: underline;
   }
