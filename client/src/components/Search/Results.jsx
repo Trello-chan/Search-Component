78e9a0249b86766d4eea3baeb1aaa284517d6290
Kin Chan <xkinchanx@gmail.com>
2019-01-05 01:00:43 -0800

    [prog] rendering card data, need to add label click functionality next + hover CSS

--- client/src/components/Search/Results.jsx
+++ client/src/components/Search/Results.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Results = ({ cards }) =>
   <div>
@@ -9,10 +10,46 @@ const Results = ({ cards }) =>
       </div>
     :
       <div>
-        mapping thru cards
+        {cards.map(card => 
+          <CardContainer key={card.id}>
+            <div>
+              {card.label}
+            </div>
+            <div>
+              <div><b>{card.label}</b></div>
+              <div>in <b>{card.list}</b> on <b>{card.boardId}</b></div>
+            </div>
+          </CardContainer>
+        
+        )}
       </div>
     
     }
   </div>
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding-bottom: 20px;
+  > :nth-child(1) {
+    background: white;
+    border-radius: 2px;
+    color: #17394d;
+    display: flex;
+    align-items: center;
+    margin-right: 10px;
+    padding: 5px;
+    width: 155px;
+  }
+  > :nth-child(2) {
+    > :nth-child(1) {
+      color: #17394d;
+      & :hover {
+        cursor: pointer;
+        text-decoration: underline;
+      }
+    }
+  }
+`
 
 export default Results;
