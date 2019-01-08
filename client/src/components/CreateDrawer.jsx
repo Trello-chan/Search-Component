72586f4d1ae45fac7fc4168c5ba3cc155b0920a4
Kin Chan <xkinchanx@gmail.com>
2019-01-07 16:49:54 -0800

    [prog] createdrawer with some styling

---
+++ client/src/components/CreateDrawer.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+
+class CreateDrawer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <DrawerContainer>
+        <div>
+          <div>Create</div>
+          <div>X</div>
+        </div>
+        
+        <hr/>
+
+        <div>
+          <div>Create Board...</div>
+          <div>description</div>
+        </div>
+
+        <div>
+          <div>Create Team...</div>
+          <div>description</div>
+        </div>
+
+        <div>
+          <div>Create Business Team...</div>
+          <div>description</div>
+        </div>
+      </DrawerContainer>
+    )
+  }
+}
+
+const DrawerContainer = styled.div`
+  background: white;
+  border-radius: 2px;
+  position: absolute;
+  right: 5px;
+  top: 26px;
+  width: 300px;
+  > :nth-child(1){
+    display: flex;
+    flex-direction: row;
+    > :nth-child(1) {
+      width: 90%;
+    }
+    > :nth-child(2) {
+      width: 10%;
+    }
+  }
+`;
+
+export default CreateDrawer;
