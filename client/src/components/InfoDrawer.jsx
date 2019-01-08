7afa341c58006c66cf2c973764a50a0b07fef2e4
Kin Chan <xkinchanx@gmail.com>
2019-01-08 13:18:31 -0800

    [prog] rendering actual trello images + linkes

--- client/src/components/InfoDrawer.jsx
+++ client/src/components/InfoDrawer.jsx
@@ -5,11 +5,34 @@ class InfoDrawer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      messages: [
+        {
+          message: 'New To Trello-chan? Check Out The Guide',
+          image: 'https://a.trellocdn.com/prgb/dist/images/tips/guide-1@1x.3a63a6244ef2e7ade4d6.png',
+          link: 'https://trello.com/guide?utm_source=trello&utm_medium=inapp&utm_content=header-tips&utm_campaign=guide'
+        },
+        {
+          message: 'Get Inspired By Dozens Of Different Trello-chan Workflows',
+          image: 'https://a.trellocdn.com/prgb/dist/images/tips/inspiration-1@1x.e1565bd541d2b56ec792.png',
+          link: 'https://trello.com/inspiration?utm_source=trello&utm_medium=inapp&utm_content=header-tips&utm_campaign=inspiration'
+        },
+        {
+          message: 'Make Boards More Powerful With Trello-chan Power-Ups',
+          image: 'https://a.trellocdn.com/prgb/dist/images/tips/power-ups-1@1x.d7178bb1e5dc48fb4fe6.png',
+          link: 'https://trello.com/power-ups?utm_source=trello&utm_medium=inapp&utm_content=header-tips&utm_campaign=power-ups'
+        },
+        {
+          message: 'Join A Webinar To Take Your Productivity To The Next Level',
+          image: 'https://a.trellocdn.com/prgb/dist/images/tips/webinars-1@1x.7e1a1e13b5deffa70d0e.png',
+          link: 'https://trello.com/webinars?utm_source=trello&utm_medium=inapp&utm_content=header-tips&utm_campaign=webinar'
+        }
+      ],
+      index: 0
     }
   }
 
   render() {
+    let { index, messages } = this.state;
     return (
       <DrawerContainer>
         <div>
@@ -19,11 +42,11 @@ class InfoDrawer extends Component {
         
         <hr/>
 
-        <img src="https://a.trellocdn.com/prgb/dist/images/tips/guide-1@1x.3a63a6244ef2e7ade4d6.png 1x, https://a.trellocdn.com/prgb/dist/images/tips/guide-1@2x.eab001f563b4e3d8a5d1.png 2x" alt=""/>
-
-        <h5>New To Trello? Check Out The Guide</h5>
-
-        <div><u>Get a new tip</u></div>
+        <div>
+          <img src={messages[index].image} alt=""/>
+          <h5>{messages[index].message}</h5>
+          <div><u>Get a new tip</u></div>
+        </div>
 
         <hr />
 
@@ -46,29 +69,37 @@ const DrawerContainer = styled.div`
   color: #6b808c;
   font-family: Helvetica Neue,Arial,Helvetica,sans-serif;
   font-size: 9.5px;
+  height: 270px;
   padding-top: 10px;
   padding-bottom: 10px;
   position: absolute;
   right: 10px;
   top: 33px;
-  width: 240px;
-  > :nth-child(n + 3) {
-    padding: 5px 10px;
-    &:hover {
-      background: #0094FF;
-      color: white;
+  width: 310px;
+  > :nth-child(n+3) {
+    text-align: center;
+    > :nth-child(n) {
+      &:hover {
+        background-color: rgba(210,210,210,.35);
+        color: black;
+      }
+    }
+  }
+  > :nth-child(5) {
+    margin: 10px 0px;
+    > :nth-child(n) {
+      margin: 0px 10px;
     }
   }
   & h5 {
     color: #17394d;
-    font-size: 12px;
-    margin: 0px 0px 5px 0px;
+    font-size: 14px;
+    margin: 5px 0px 5px 0px;
   }
   > :nth-child(1){
     display: flex;
     flex-direction: row;
-    padding-left: 10px;
-    padding-right: 10px;
+    padding: 0px 10px 5px 10px;
     > :nth-child(1) {
       font-size: 12px;
       line-height: 8px;
