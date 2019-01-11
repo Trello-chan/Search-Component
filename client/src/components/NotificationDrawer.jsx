38b159cf2731d5f1b9015761171cb6c05f4e46cf
Kin Chan <xkinchanx@gmail.com>
2019-01-11 15:58:25 -0800

    [refactor] notifications drawer resizing

--- client/src/components/NotificationDrawer.jsx
+++ client/src/components/NotificationDrawer.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const InfoDrawer = () =>
+const InfoDrawer = ({ closeDisplay }) =>
   <DrawerContainer>
     <div>
       <div>Notifications</div>
-      <div>X</div>
+      <div onClick={closeDisplay}>&#215;</div>
     </div>
 
     <hr/>
@@ -18,48 +18,69 @@ const InfoDrawer = () =>
         <div>No Unread Notifications</div>
         <div>Click <u>View All</u> to view all your notifications</div>
       </div>
-
-      <div>
-        <h5>Change Notification Email Frequency</h5>
-      </div>
     </DrawerContent>
 
+    <Options>
+      <h4>Change Notification Email Frequency</h4>
+    </Options>
   </DrawerContainer>
 
 const DrawerContainer = styled.div`
   background: white;
   border-radius: 2px;
   color: #6b808c;
-  font-family: Helvetica Neue,Arial,Helvetica,sans-serif;
-  font-size: 9.5px;
-  height: 270px;
+  max-height: 520px;
   padding-top: 10px;
   padding-bottom: 10px;
   position: absolute;
   right: 10px;
-  top: 33px;
-  width: 310px;
+  top: 41px;
+  width: 420px;
   > :nth-child(1){
     display: flex;
     flex-direction: row;
-    padding: 0px 10px 5px 10px;
+    padding: 5px 10px;
     > :nth-child(1) {
-      font-size: 12px;
-      line-height: 8px;
       margin-left: 20px;
       text-align: center;
       width: 90%;
     }
     > :nth-child(2) {
+      font-size: 20px;
+      line-height: 12px;
+      font-weight: 300;
       text-align: end;
       width: 10%;
+      &:hover { 
+        cursor: pointer;
+      }
     }
   }
 `;
 
 const DrawerContent = styled.div`
+  margin: 10px;
   & u {
-    cursor: pointer
+    cursor: pointer;
+    color: black;
+  }
+  > :nth-child(2) {
+    height: 250px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    > img {
+      padding: 30px;
+    }
+    > :nth-child(2) {
+      font-size: 16px;
+      padding: 10px;
+    }
+    > :nth-child(3) {
+      font-size: 12px;
+    }
   }
   > :nth-child(3) {
     cursor: pointer;
@@ -67,6 +88,19 @@ const DrawerContent = styled.div`
       background-color: rgba(210,210,210,.35);
       color: white;
     }
+  }
+`;
+
+const Options = styled.div`
+  margin: 80px 0px 40px 0px;
+  color: #17394d;
+  > h4 { 
+    padding: 10px;
+  }
+  & :hover {
+    color: white;
+    background: #3F7FBF;
+    cursor: pointer;
   }
 `
 
