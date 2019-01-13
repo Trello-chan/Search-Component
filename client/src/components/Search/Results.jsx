1ef40507ae0584e7b5f7e1d856fac3089caf5bb7
Kin Chan <xkinchanx@gmail.com>
2019-01-12 21:30:33 -0800

    [refactor + prog] changing to class and adding onMouseEnter/Leave events to see if we can imitate Trello + making other cards smaller on hover

--- client/src/components/Search/Results.jsx
+++ client/src/components/Search/Results.jsx
@@ -1,31 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
-const Results = ({ cards }) =>
-  <div>
-    {cards.length === 0 ? 
-    
-      <Center>
-        We couldn't find any cards or boards that matched your search.
-      </Center>
-    :
-      <div>
-        {cards.map(card => 
-          <CardContainer key={card.id}>
-            <div>
-              {card.label}
-            </div>
-            <div>
-              <div><b>{card.label}</b></div>
-              <div>in <b>{card.list}</b> on <b>{card.boardId}</b></div>
-            </div>
-          </CardContainer>
-        
-        )}
-      </div>
-    
+class Results extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hoverOveringID: null
     }
-  </div>
+  }
+
+  //if null, all cards show normally
+  //if not null and card is not the state card.id, then it should be smaller
+  hoveringOverCard = (hoverOveringID) => this.setState({ hoverOveringID }, () => console.log(this.state));
+
+  render() {
+    let { hoverOveringID } = this.state;
+    let { cards } = this.props;
+    return (
+    <div>
+      {cards.length === 0 ? 
+      
+        <Center>
+          We couldn't find any cards or boards that matched your search.
+        </Center>
+      :
+        <div>
+          {cards.map(card => 
+            <CardContainer key={card.id}>
+              <div 
+                onMouseEnter={() => this.hoveringOverCard(card.id)} 
+                onMouseLeave={() => this.hoveringOverCard(null)}
+                >
+                {card.label}
+              </div>
+              <div>
+                <div><b>{card.label}</b></div>
+                <div>in <b>{card.list}</b> on <b>{card.boardId}</b></div>
+              </div>
+            </CardContainer>
+          )}
+        </div>
+      }
+    </div>
+    )
+  }
+}
 
 const CardContainer = styled.div`
   display: flex;
@@ -42,6 +61,9 @@ const CardContainer = styled.div`
     margin-right: 10px;
     padding: 5px;
     width: 195px;
+    &:hover {
+      cursor: pointer;
+    }
   }
   > :nth-child(2) {
     > :nth-child(1) {
