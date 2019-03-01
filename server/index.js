40cfe0ae54c2a55b396a7d857f33942355ed2c5e
Kin Chan <xkinchanx@gmail.com>
2019-02-28 22:51:24 -0800

    [fix] temp fix, aws s3 bucket not allowing public access, providing bundles via servers, converging at proxy

--- server/index.js
+++ server/index.js
@@ -1,19 +1,22 @@
 import express from 'express';
 import path from 'path';
 import parser from 'body-parser';
+import cors from 'cors';
 
 import router from './router';
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.use('/api', router)
 
-app.use('*', (req, res) => res.sendFile(path.resolve(__dirname, '../client/dist/index.html')));
+// app.use('*', (req, res) => res.sendFile(path.resolve(__dirname, '../client/dist/index.html')));
+app.get('/bundle', (req, res) => res.sendFile(path.resolve(__dirname, '../client/dist/bundle.js')));
 
 if (process.env.NODE_ENV !== 'jest') {
   app.listen(port, () => console.log(`server is listening on port ${port}`));
