52528cf75d1a985404d1bc4e2a7883a3c173372c
Kin Chan <xkinchanx@gmail.com>
2019-03-11 20:10:49 -0700

    [prog] configs for deployment and connecting to DB

--- database/index.js
+++ database/index.js
@@ -2,9 +2,26 @@ import Sequelize from 'sequelize';
 
 let SQL_connection;
 
-if (process.env.DATABASE) {
-  SQL_connection = new Sequelize(process.env.DATABASE);
+if (process.env.ENV === "production") {
+  console.log(`there's a DATABASE`);
+  SQL_connection = new Sequelize(process.env.DB_NAME, process.env.USER, process.env.PASSWORD, {
+    host: process.env.DATABASE,
+    port: 5432,
+    logging: console.log,
+    maxConcurrentQueries: 100,
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl:'Amazon RDS'
+    },
+    pool: { maxConnections: 5, maxIdleTime: 30},
+    define: {
+      timestamps: false
+    },
+    language: 'en'
+  })
+
 } else {
+  console.log('no process.env.DATABASE');
   SQL_connection = new Sequelize('trello_search', process.env.USERNAME || 'root', process.env.PW || '', {
     host: process.env.host || 'localhost',
     dialect: 'postgres',
